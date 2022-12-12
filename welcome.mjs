7838a6235a4f0eb67ffcb61dca17b9409d07dc73
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-12-12 08:15:44 +0100

    Create welcome.mjs

---
+++ welcome.mjs
@@ -0,0 +1,8 @@
+const welcome = (request, response) => {
+    return response.json({
+      message: "Welcome to the lobby ! ",
+      user: { id: request.userId, email: request.userEmail, expire: request.exp },
+    });
+  }
+
+export default welcome
