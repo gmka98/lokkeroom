dfe3067a1aed333d09c85b237999537cc26c600b
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-12-19 09:44:40 +0100

    Delete Controllers directory

--- Controllers/logout.mjs
+++
@@ -1,8 +0,0 @@
-const logout = (request, response) => {
-  return response
-    .clearCookie("access_token")
-    .status(200)
-    .json({ message: "Successfully logged out" });
-};
-
-export default logout
