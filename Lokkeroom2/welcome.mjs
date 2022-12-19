4cc058c951d7cc01f8c76a117726357a3a65b7cb
Gael Mukendi Kabongo <111666680+gmka98@users.noreply.github.com>
2022-12-19 09:47:46 +0100

    Delete Lokkeroom2 directory

--- Lokkeroom2/welcome.mjs
+++
@@ -1,9 +0,0 @@
-const welcome = (request, response) => {
-    return response.json({
-      message: "Welcome to the lobby ! ",
-      user: { id: request.userId, email: request.userEmail, expire: request.exp },
-    });
-  }
-
-export default welcome
-
